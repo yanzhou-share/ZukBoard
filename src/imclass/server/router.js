bfdd07518fd0ad36b1e05acd3ba671c7af1ddcaf
congfeng <msi_008@163.com>
2018-11-07 17:07:54 +0800

    修改代码

--- src/imclass/server/router.js
+++ src/imclass/server/router.js
@@ -1,3 +1,7 @@
+const configAll = require('../../../config')
+const env = process.env.NODE_ENV || 'development'
+const config = configAll.ENV_VAR[env]
+
 module.exports = {
   'get#/': async ctx => {
     return ctx.redirect('/app/canvas/draw')
@@ -9,6 +13,7 @@ module.exports = {
     ctx.session.originalUrl = ctx.originalUrl
     await ctx.render('layout/index.html', {
       title: '',
+      baseUrl: config.baseUrl,
       dataset: encodeURIComponent(JSON.stringify({
         user: ctx.session.user,
         biUrl: ctx.state.biUrl
