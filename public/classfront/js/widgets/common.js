31e3f9a59ede2919f30d3d8d8234c430203f111b
f <f@fdeMac-mini.local>
2018-11-08 17:56:58 +0800

    delete dist

--- public/classfront/js/widgets/common.js
+++
@@ -1,9 +0,0 @@
-$(window).on("load resize",function(){
-    var h=window.innerHeight||document.body.clientHeight||document.documentElement.clientHeight;
-    if($('.scroll-header').length > 0) {
-        $('.scroll-header').css("height", (h-250)+'px');
-    }
-    if($('.scroll').length > 0) {
-        $('.scroll').css("height", (h-203)+'px');
-    }
-});
