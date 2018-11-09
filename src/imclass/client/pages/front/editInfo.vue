7f6e6041eaae9a1e53c7817fad30a1d056891c35
f <f@fdeMac-mini.local>
2018-11-09 11:21:02 +0800

    add logout and loading upload

---
+++ src/imclass/client/pages/front/editInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="layer_modify cf bg_fff f-14">
+        <dl>
+        <dt class="txt_color_666">修改姓名</dt>
+        <dd><div class="inp"><input name="" v-model="name" class="f-14" type="text"></div></dd>
+        <dd class="f-right">
+        <input name="" class="f-14 txt_color_777" value="取消" @click="cancel" type="reset">
+        <input name="" class="f-14 txt_color_d9" value="保存" @click="save" type="button">
+        </dd>
+        </dl>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    cancel: function () {
+
+    },
+    save: function () {
+
+    }
+  }
+}
+</script>
+    
---
+++ src/imclass/client/pages/front/userInfo.vue
@@ -0,0 +1,31 @@
+</<template>
+    <div class="layer_login layer_login2 f-14 txt_color_777 clearfix">
+        <ul>
+            <li><img src="../../assets/images/ico_user.svg"><span v-text="mobile"></span></li>
+            <li @click="logout"><span><img src="../../assets/images/ico_exit.svg">退出登录</span></li>
+        </ul> 
+    </div>    
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props: ['mobile'],
+  methods: {
+    logout: () => {
+      window.localStorage.clear()
+      window.location.href = '/imclass/front/login'
+    }
+  }
+}
+</script>
+
+<style>
+.clearfix {
+    clear: both;
+}
+</style>
+
