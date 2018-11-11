ac2dc5e4d5d82770a0a7d77a0b5090e49661e499
congfeng <msi_008@163.com>
2018-11-11 17:16:06 +0800

    修改代码

--- src/imclass/client/components/classin/VideoContent.vue
+++ src/imclass/client/components/classin/VideoContent.vue
@@ -165,6 +165,11 @@ export default {
       }
 
       this.participants = Array.from(room.participants.values())
+      // Array.from(room.participants.values()).forEach((item) => {
+      //   if (!this.findUser(item)) {
+      //     that.participants.push(item)
+      //   }
+      // })
 
       // 加载远端视频 Attach the Tracks of the Room's Participants.
       //            room.participants.forEach(function(participant) {
@@ -181,7 +186,6 @@ export default {
           return
         }
         that.participantAddTrack(participant, track)
-        console.log(that.participants.tracks)
       })
 
       // When a Participant removes a Track, detach it from the DOM.
